aaf6780e3a2ee218dd1e2c223495d245d074f862
kholood980 <kholoodabdalrahman22@gmail,com>
2025-03-17 14:17:50 +0400

    add div in index.cshtml

---
+++ WebApplication1/WebApplication1/Controllers/StudentController1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController1 : Controller
+    {
+        List<Student> students = new List<Student>()
+        { new Student() { Id = 2, Name = "salem", Age = 22, Semester = "three" },
+         new Student() { Id = 2, Name = "salem", Age = 22, Semester = "three" },
+         new Student() { Id = 2, Name = "salem", Age = 22, Semester = "three" },
+         new Student() { Id = 2, Name = "salem", Age = 22, Semester = "three" }};
+        dept dep = new dept() { Id = 3, Name_dept = "IT" };
+
+        public IActionResult Index()
+        {
+             Student d = new Student();
+             d.Id = 1;
+             d.Name = "salem";
+             d.Age = 22;
+             d.Semester = "three";
+             ViewBag.dp = dep;
+             ViewBag.st=d; ;  
+             return View();
+        }
+        public IActionResult display()
+        {
+            
+            return View(students);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/dept.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models
+{
+    public class dept
+    {
+        public int Id { get; set; } 
+        public string Name_dept { get; set; }
+    }
+}
